c02e2d61459ab959da63de010e5798086367065b
Rajesh Madamanchi <rmadamanchi@gmail.com>
2019-12-16 08:45:22 -0600

    remove aws resource

--- main.tf
+++ main.tf
@@ -1,33 +1,3 @@
 resource "null_resource" "dummy" {
 
 }
-
-provider "aws" {
-  profile    = "default"
-  region     = "us-east-1"
-}
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.nano"
-
-  tags = {
-    Name = "HelloWorld"
-  }
-}
