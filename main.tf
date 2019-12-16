2cf748c38f1be8c3e23d3dfee1c82b86fe2f8a2b
Rajesh Madamanchi <rmadamanchi@gmail.com>
2019-12-16 10:19:17 -0600

    use module

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
 resource "null_resource" "dummy" {
 
 }
+
+module "hello-ec2" {
+  source = "github.com/rmadamanchi/tf-hello"
+  # source = "../tf-module-ec2"
+  Name = "HelloWorld"
+  Region = "us-east-2"
+}
